c42577eb4288c5b74ef0481bfd033b539d0ef69b
Saïd SIDER <sider.said@neuf.fr>
2020-11-21 11:27:18 +0100

    initial commit

---
+++ app/src/main/java/com/example/partageparintent/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.partageparintent
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnShowToast.setOnClickListener {
+            Log.i("MainActivity", "Button was clicked !")
+            Toast.makeText(this, "Button was clicked !", Toast.LENGTH_SHORT).show()
+        }
+
+        btnSendMsgToNextActivity.setOnClickListener {
+            val message: String = etUserMessage.text.toString()
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
